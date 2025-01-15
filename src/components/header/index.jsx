11832b83a05a5119b278b5db86de5f97c829d0a6
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-01-15 14:32:38 -0300

    Finalizei o básico do header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
         <div>
-            <h1>PROJETO DE CORRIDA</h1>
+            <h1 className='text-center bg-red-500 p-3 text-white font-bold text-lg'>PROJETO DE CORRIDA</h1>
         </div>
     )
 }
