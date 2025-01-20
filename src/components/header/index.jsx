67ccd41d867c5ac018a6f8a9ab08409758bd1952
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-01-20 14:40:28 -0300

    Adicionei as bandeiras no titulo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,11 @@ export default function Header() {
     return (
         <div className='flex items-center justify-center h-16 fixed top-0 left-0 w-full bg-red-500 border-b border-white shadow-lg'>
             <Link href={"/"}>
-                <h1 className='text-center text-white font-bold text-lg'>PROJETO DE CORRIDA</h1>
+                <div className='flex'>
+                    <img src="/images/bandeira.png" alt="bandeira" className='h-8' />
+                    <h1 className='text-center text-white font-bold text-lg ml-6 mr-6'>PROJETO DE CORRIDA</h1>
+                    <img src="/images/bandeira.png" alt="bandeira" className='h-8' />
+                </div>
             </Link>
         </div>
     )
