3540bc3b345880ea00b59f1a5e774ce1dd5f5571
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-02-19 18:09:24 -0300

    Comecei a criar uma outra página para testar um cenário de ter mais de um jogo, sendo uma página para cada jogo

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,7 +1,7 @@
 export default function Game() {
     return (
-        <div className="h-full">
-            <iframe src="/page.html" width="100%" height="100%"></iframe>
+        <div className="h-[90%] w-[90%] flex justify-center items-center">
+            <iframe src="/teste.html" className="w-full h-full" style={{ maxWidth: '100%', maxHeight: '100%' }}></iframe>
         </div>
     )
 }
