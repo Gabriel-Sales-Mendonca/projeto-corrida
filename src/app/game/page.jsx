f1c937082148020d6a87c4588c172c12a2e12cae
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-02-19 18:54:43 -0300

    Criei um arquivo apenas para fazer a responsividade do jogo e retirei os elementos que não devem aparecer no celular

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,7 +1,7 @@
 export default function Game() {
     return (
-        <div className="h-[90%] w-[90%] flex justify-center items-center">
-            <iframe src="/teste.html" className="w-full h-full" style={{ maxWidth: '100%', maxHeight: '100%' }}></iframe>
+        <div className="h-[100%] w-[100%] flex justify-center items-center">
+            <iframe src="/page.html" className="w-full h-full" style={{ maxWidth: '100%', maxHeight: '100%' }}></iframe>
         </div>
     )
 }
